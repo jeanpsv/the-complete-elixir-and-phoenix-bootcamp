e8705c38eb3835e6f3adbbb2f0d3a96ef175c03c
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2017-05-28 13:56:01 -0300

    generate cards project

---
+++ cards/lib/cards.ex
@@ -0,0 +1,18 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for Cards.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
