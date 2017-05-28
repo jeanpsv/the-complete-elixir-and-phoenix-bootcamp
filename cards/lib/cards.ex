3e07bd4a20299665ef0bcacd3799dd35827fd3f8
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2017-05-28 17:43:49 -0300

    improve create_cards

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -17,7 +17,19 @@ defmodule Cards do
   end
 
   def create_deck do
-  	["Ace", "Two", "Three"]
+  	values = ["Ace", "Two", "Three", "Four", "Five"]
+  	suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+  	# cards = for value <- values do
+  	# 	for suit <- suits do
+  	# 		"#{value} of #{suit}"
+  	# 	end
+  	# end
+  	# List.flatten(cards)
+  	
+  	for suit <- suits, value <- values do
+  		"#{value} of #{suit}"
+  	end
   end
 
   def shuffle(deck) do
