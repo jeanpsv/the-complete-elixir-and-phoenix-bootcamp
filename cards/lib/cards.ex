15385713fa6aa2bc609ae17ae01ef1d804c37e19
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2017-05-31 12:42:55 -0300

    improve load method

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -50,10 +50,9 @@ defmodule Cards do
   end
 
   def load(filename) do
-  	{status, binary} = File.read(filename)
-  	case status do
-  		:ok -> :erlang.binary_to_term(binary)
-  		:error -> "That file does not exist"
+  	case File.read(filename) do
+  		{:ok, binary} -> :erlang.binary_to_term binary
+  		{:error, _} -> "That file does not exist"
   	end
   end
 end
