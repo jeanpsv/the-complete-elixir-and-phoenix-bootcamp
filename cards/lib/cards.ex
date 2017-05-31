05dae01efb6b5e083b24d4bf02f2147e942af0e5
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2017-05-30 22:23:48 -0300

    implement load method

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -48,4 +48,9 @@ defmodule Cards do
   	binary = :erlang.term_to_binary(deck)
   	File.write(filename, binary)
   end
+
+  def load(filename) do
+  	{status, binary} = File.read(filename)
+  	:erlang.binary_to_term(binary)
+  end
 end
