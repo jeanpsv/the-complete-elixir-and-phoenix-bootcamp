55b25d2bec723140bea74c676ddd5e95560ba12a
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2017-06-02 07:58:03 -0300

    remove hello world method

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -2,17 +2,4 @@ defmodule Identicon do
   @moduledoc """
   Documentation for Identicon.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Identicon.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
