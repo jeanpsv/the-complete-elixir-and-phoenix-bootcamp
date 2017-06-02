e3cc563cb5200cbe2ab10b510157bc24b316a152
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2017-06-02 08:59:36 -0300

    implementing identicon

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -2,4 +2,22 @@ defmodule Identicon do
   @moduledoc """
   Documentation for Identicon.
   """
+
+  def main(input) do
+  	input
+  	|> hash_input
+  	|> pick_color
+  end
+
+  def pick_color(image) do
+  	%Identicon.Image{hex: hex_list} = image
+  	[r, g, b | _tail] = hex_list
+  	[r, g, b]
+  end
+
+  def hash_input(input) do
+  	hex = :crypto.hash(:md5, input)
+  	|> :binary.bin_to_list
+  	%Identicon.Image{hex: hex}
+  end
 end
