62240ed7252a41175b52f272ed975218d298f350
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2017-06-04 20:09:41 -0300

    build pixel map

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -9,6 +9,7 @@ defmodule Identicon do
   	|> pick_color
   	|> build_grid
   	|> filter_odd_squares
+  	|> build_pixel_map
   end
 
 
@@ -38,9 +39,24 @@ defmodule Identicon do
   	row ++ [ second, first ]
   end
 
-  def filter_odd_squares(%Identicon.Image{grid: grid} = _image) do
-  	Enum.filter grid, fn({ code, _index }) ->
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+  	grid = Enum.filter grid, fn({ code, _index }) ->
   		rem(code, 2) == 0
   	end
+
+  	%Identicon.Image{image | grid: grid}
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+  	pixel_map = Enum.map grid, fn({ _code, index }) ->
+  		horizontal = rem(index, 5) * 50
+  		vertical = div(index, 5) * 50
+  		top_left = { horizontal, vertical }
+  		bottom_right = { horizontal + 50, vertical + 50 }
+
+  		{ top_left, bottom_right }
+  	end
+
+  	%Identicon.Image{image | pixel_map: pixel_map}
   end
 end
