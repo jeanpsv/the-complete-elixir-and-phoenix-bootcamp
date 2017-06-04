8658fc53f64aa91929f34cd4efb31cecbbac692b
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2017-06-04 20:17:59 -0300

    implement draw image method

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -10,6 +10,7 @@ defmodule Identicon do
   	|> build_grid
   	|> filter_odd_squares
   	|> build_pixel_map
+  	|> draw_image
   end
 
 
@@ -58,5 +59,16 @@ defmodule Identicon do
   	end
 
   	%Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+  	image = :egd.create(250, 250)
+  	fill = :egd.color(color)
+
+  	Enum.each pixel_map, fn({ start, stop }) ->
+  		:egd.filledRectangle(image, start, stop, fill)
+  	end
+
+  	:egd.render(image)
   end
 end
