831b82b4b8ed5c1049b581a61d8ec22f61907644
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2017-06-04 16:03:35 -0300

    refactor sign method

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -9,8 +9,7 @@ defmodule Identicon do
   	|> pick_color
   end
 
-  def pick_color(image) do
-  	%Identicon.Image{hex: [r, g, b | _tail]} = image
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
   	%Identicon.Image{image | color: {r, g, b}}
   end
 
