ce1c5a236b4292ba0884cbf81cf87003d3fc5289
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2017-06-10 23:27:29 -0300

    implementin topic controller index and create actions

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -3,9 +3,28 @@ defmodule Discuss.TopicController do
 
 	alias Discuss.Topic
 
-	def new(conn, params) do
+	def index(conn, _params) do
+		topics = Repo.all(Topic)
+
+		render conn, "index.html", topics: topics
+	end
+
+	def new(conn, _params) do
 		changeset = Topic.changeset(%Topic{}, %{})
 
 		render conn, "new.html", changeset: changeset
-	end	
+	end
+
+	def create(conn, %{"topic" => topic}) do
+		changeset = Topic.changeset(%Topic{}, topic)
+
+		case Repo.insert(changeset) do
+			{:ok, post} ->
+				conn
+				|> put_flash(:info, "Topic Created")
+				|> redirect(to: topic_path(conn, :index))
+			{:error, changeset} ->
+				render conn, "new.html", changeset: changeset
+		end
+	end
 end
