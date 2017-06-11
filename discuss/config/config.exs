e09e1100761e0ec062f2a56ce7fc2628d6d4a444
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2017-06-11 15:26:14 -0300

    fix typo

--- discuss/config/config.exs
+++ discuss/config/config.exs
@@ -27,7 +27,7 @@ config :ueberauth, Ueberauth,
 		github: { Ueberauth.Strategy.Github, [] }
 	]
 
-config :ueberauth, Ueberauth.Strategy.Github.Oauth,
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
 	client_id: System.get_env("GITHUB_CLIENT_ID"),
 	client_secret: System.get_env("GITHUB_CLIENT_SECRET")
 
