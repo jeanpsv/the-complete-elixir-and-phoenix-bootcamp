4af3f1796161fbffea816fba59217abb91181184
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2017-06-10 23:27:42 -0300

    add topic index on route

--- discuss/web/router.ex
+++ discuss/web/router.ex
@@ -17,6 +17,8 @@ defmodule Discuss.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+
+    get "/topics", TopicController, :index
     get "/topics/new", TopicController, :new
     post "/topics", TopicController, :create
   end
