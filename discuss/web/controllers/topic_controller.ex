c22c08f549033cacb692b56f24dc725f973497fd
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2017-06-10 21:40:57 -0300

    implemets new topic controller action

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -1,7 +1,11 @@
 defmodule Discuss.TopicController do
 	use Discuss.Web, :controller
 
+	alias Discuss.Topic
+
 	def new(conn, params) do
-		
+		changeset = Topic.changeset(%Topic{}, %{})
+
+		render conn, "new.html", changeset: changeset
 	end	
 end
