f7c800dd133fa13cc809c15f522d4b016dc8d051
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2017-06-11 10:30:31 -0300

    implements topic edit

--- discuss/web/router.ex
+++ discuss/web/router.ex
@@ -21,6 +21,8 @@ defmodule Discuss.Router do
     get "/topics", TopicController, :index
     get "/topics/new", TopicController, :new
     post "/topics", TopicController, :create
+    get "/topics/:id/edit", TopicController, :edit
+    put "/topics/:id", TopicController, :update
   end
 
   # Other scopes may use custom stacks.
