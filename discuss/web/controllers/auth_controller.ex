1678bab191689d8f8259c2d1f122b91652f65f1e
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2017-06-11 17:47:44 -0300

    implement github oauth strategy authentication

--- discuss/web/controllers/auth_controller.ex
+++ discuss/web/controllers/auth_controller.ex
@@ -2,11 +2,38 @@ defmodule Discuss.AuthController do
 	use Discuss.Web, :controller
 	plug Ueberauth
 
-	def callback(conn, params) do
-		IO.puts "++++++++++++++++++++"
-		IO.inspect(conn.assigns)
-		IO.puts "++++++++++++++++++++"
-		IO.inspect(params)
-		IO.puts "++++++++++++++++++++"
+	alias Discuss.User
+
+	def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+		user_params = %{
+			email: auth.info.email,
+			provider: "github",
+			token: auth.credentials.token
+		}
+		changeset = User.changeset(%User{}, user_params)
+		signin(conn, changeset)
+	end
+
+	defp signin(conn, changeset) do
+		case insert_or_update_user(changeset) do
+			{:ok, user} ->
+				conn
+				|> put_flash(:info, "Welcome back!")
+				|> put_session(:user_id, user.id)
+				|> redirect(to: topic_path(conn, :index))
+			{:error, _reason} ->
+				conn
+				|> put_flash(:error, "Error signing in")
+				|> redirect(to: topic_path(conn, :index))
+		end
+	end
+
+	defp insert_or_update_user(changeset) do
+		case Repo.get_by(User, email: changeset.changes.email) do
+			nil ->
+				Repo.insert(changeset)
+			user ->
+				{:ok, user}
+		end
 	end
 end
