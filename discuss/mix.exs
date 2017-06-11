7f119d2e98ff01d241b8a3323e08e6682ff1fec3
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2017-06-11 14:32:39 -0300

    add mix dependencies

--- discuss/mix.exs
+++ discuss/mix.exs
@@ -2,24 +2,38 @@ defmodule Discuss.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :discuss,
-     version: "0.0.1",
-     elixir: "~> 1.2",
-     elixirc_paths: elixirc_paths(Mix.env),
-     compilers: [:phoenix, :gettext] ++ Mix.compilers,
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     aliases: aliases(),
-     deps: deps()]
+  	[
+			app: :discuss,
+			version: "0.0.1",
+			elixir: "~> 1.2",
+			elixirc_paths: elixirc_paths(Mix.env),
+			compilers: [:phoenix, :gettext] ++ Mix.compilers,
+			build_embedded: Mix.env == :prod,
+			start_permanent: Mix.env == :prod,
+			aliases: aliases(),
+			deps: deps()
+  	]
   end
 
   # Configuration for the OTP application.
   #
   # Type `mix help compile.app` for more information.
   def application do
-    [mod: {Discuss, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+    [
+    	mod: {Discuss, []},
+     	applications: [
+     		:phoenix,
+     		:phoenix_pubsub,
+     		:phoenix_html,
+     		:cowboy,
+     		:logger,
+     		:gettext,
+     		:phoenix_ecto,
+     		:postgrex,
+     		:ueberauth,
+     		:ueberauth_github
+			]
+   	]
   end
 
   # Specifies which paths to compile per environment.
@@ -30,14 +44,18 @@ defmodule Discuss.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.4"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.0"},
-     {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.6"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+  	[
+			{:phoenix, "~> 1.2.4"},
+			{:phoenix_pubsub, "~> 1.0"},
+			{:phoenix_ecto, "~> 3.0"},
+			{:postgrex, ">= 0.0.0"},
+			{:phoenix_html, "~> 2.6"},
+			{:phoenix_live_reload, "~> 1.0", only: :dev},
+			{:gettext, "~> 0.11"},
+			{:cowboy, "~> 1.0"},
+			{:ueberauth, "~> 0.4"},
+			{:ueberauth_github, "~> 0.4"}
+  	]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
@@ -47,8 +65,10 @@ defmodule Discuss.Mixfile do
   #
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
-    ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-     "ecto.reset": ["ecto.drop", "ecto.setup"],
-     "test": ["ecto.create --quiet", "ecto.migrate", "test"]]
+    [
+    	"ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+     	"ecto.reset": ["ecto.drop", "ecto.setup"],
+     	"test": ["ecto.create --quiet", "ecto.migrate", "test"]
+    ]
   end
 end
