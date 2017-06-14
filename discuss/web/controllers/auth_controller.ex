ca1c586417ff595b8da1ab3157ef90737b49d24a
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2017-06-14 00:11:39 -0300

    implement signout

--- discuss/web/controllers/auth_controller.ex
+++ discuss/web/controllers/auth_controller.ex
@@ -14,6 +14,12 @@ defmodule Discuss.AuthController do
 		signin(conn, changeset)
 	end
 
+	def signout(conn, _params) do
+		conn
+		|> configure_session(drop: true)
+		|> redirect(to: topic_path(conn, :index))
+	end
+
 	defp signin(conn, changeset) do
 		case insert_or_update_user(changeset) do
 			{:ok, user} ->
